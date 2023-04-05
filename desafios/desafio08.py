e3a685edb70b88c0c6e058bc7896761de81211c4
Antônio Souza <antonioaugusto1913@gmail.com>
2023-04-05 18:14:50 -0300

    aula 11 e cores

--- desafios/desafio08.py
+++ desafios/desafio08.py
@@ -6,4 +6,4 @@ dm =  metro* 10
 cem = metro * 100
 mil = metro * 1000
 
-print('A medida de {}m corresponde a: \n {}km \n {}hm \n {}dam \n {}dm \n {}cm \n {}mm'.format(metro, km, hm, dam, dm, cem, mil))
+print('\033[1;33;47mA medida de {}m corresponde a: \n {}km \n {}hm \n {}dam \n {}dm \n {}cm \n {}mm'.format(metro, km, hm, dam, dm, cem, mil))
--- desafios/desafio10.py
+++ desafios/desafio10.py
@@ -1,4 +1,4 @@
-n = int(input('Quanto você tem na sua carteira?'))
+n = int(input('Quanto você tem na sua carteira? '))
 dol = n / 5
 
-print('Você tem {}R$, então você pode comprar {:.2f}US$'.format(n, dol))
+print('\033[0;36;41mVocê tem {}R$, então você pode comprar {:.2f}US$\033[m'.format(n, dol))
